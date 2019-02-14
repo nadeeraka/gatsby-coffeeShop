4d85f0889c60d627606813171516007dc1173d48
nadeeraka <nimanthanadeeraka@outlook.com>
2019-02-14 11:05:02 +0530

    s commit

---
+++ src/components/layout.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { StaticQuery, graphql } from "gatsby";
+
+import "./layout.css";
+
+const Layout = ({ children }) => <>{children} </>;
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired
+};
+
+export default Layout;
