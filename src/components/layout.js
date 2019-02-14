489e8c8c1422289d68acd832a95ffc10e735f281
nadeeraka <nimanthanadeeraka@outlook.com>
2019-02-14 13:10:05 +0530

    3 commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,9 +2,15 @@ import React from "react";
 import PropTypes from "prop-types";
 import { StaticQuery, graphql } from "gatsby";
 
+import "./bootstrap.min.css";
 import "./layout.css";
-
-const Layout = ({ children }) => <>{children} </>;
+import Navbar from "./Global/navbar";
+const Layout = ({ children }) => (
+  <>
+    <Navbar />
+    {children}{" "}
+  </>
+);
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,8 @@ import Layout from "../components/layout";
 
 import SEO from "../components/seo";
 
+//import { FaGulp } from "react-icons/fa";
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
