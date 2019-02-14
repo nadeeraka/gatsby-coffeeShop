a865a11801074013e0a9abd057091d5a9bccc7a4
nadeeraka <nimanthanadeeraka@outlook.com>
2019-02-14 15:12:10 +0530

    nav added

--- src/components/Global/navbar.js
+++ src/components/Global/navbar.js
@@ -20,11 +20,51 @@ export default class Navbar extends Component {
     ]
   };
 
-  navbarHandler = () => {};
+  navbarHandler = () => {
+    console.log("hello");
+  };
   render() {
     return (
       <div>
-        <h1>Hello from nav</h1>
+        <nav
+          className="navbar navbar-expand-lg navbar-light bg-light
+"
+        >
+          <Link to="/" className="navbar-brand">
+            <img src={logo} alt="logo" />
+            {/* https://www.iconfinder.com/icons/185113/coffee_streamline_icon
+Creative Commons (Attribution 3.0 Unported);
+https://www.iconfinder.com/webalys */}
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+            onClick={this.navbarHandler}
+          >
+            <span className="navbar-toggler-icon" />>
+          </button>
+          <div className={this.state.css}>
+            <ul class="navbar-nav mx-auto">
+              {this.state.links.map(link => {
+                return (
+                  <li className="nav-item " key={link.id}>
+                    <Link to={link.path} className="nav-link text-capitalize">
+                      {link.text}
+                    </Link>
+                  </li>
+                );
+              })}
+              <li class="nav-item  ml-sm-5">
+                <i class="fas fa-cart-arrow-down" />{" "}
+              </li>
+            </ul>{" "}
+          </div>
+        </nav>
       </div>
     );
   }
